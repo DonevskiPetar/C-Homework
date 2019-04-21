99bce6ae8c9828b03ac4a7b49619586b0e54cb4f
DonevskiPetar <donevskipetar@gmail.com>
2019-04-21 23:59:36 +0200

    Delegates - BreakingNewsApp

---
+++ BreakingNewsApp.cs/MyLibrary/Classes/Follower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Classes
+{
+    public class Follower
+    {
+        public string Name { get; set; }
+        public Follower (string name)
+        {
+            Name = name;
+        }
+
+        public void Subscribe(Medium medium)
+        {
+            medium.EventBreakingNews += new Medium.NewsDelegate(PrintNews);
+            Console.WriteLine($"{this.Name} is subscribed");
+        }
+
+        public void PrintNews(object obj , NewsEventArgs BreakingNews)
+        {
+            Console.WriteLine($"{BreakingNews.Title} {BreakingNews.Content} {BreakingNews.Date}");
+        }
+
+    }
+}
---
+++ BreakingNewsApp.cs/MyLibrary/Classes/Medium.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Classes
+{
+    public class Medium
+    {
+        public delegate void NewsDelegate(object obj , NewsEventArgs e);
+        public event NewsDelegate EventBreakingNews;
+
+        public void SendBreakingNews(string title , string content , DateTime date)
+        {
+            NewsEventArgs BreakingNews = new NewsEventArgs(title, content, date);
+            if(EventBreakingNews == null)
+            {
+                Console.WriteLine("There is no subscribers");
+                return;
+            }
+            EventBreakingNews(this, BreakingNews);
+        }
+    }
+
+    public class NewsEventArgs : EventArgs
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        public NewsEventArgs(string title , string content , DateTime date)
+        {
+            Title = title;
+            Content = content;
+            Date = date;
+        }
+    }
+    
+}
